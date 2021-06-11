d69a971f190d9d00daf584460369225afb3c1339
Your Name <Alex@gmail.com>
2021-06-11 10:44:19 +0300

    Задан

--- Задания/zadan2.py
+++ Задания/zadan2.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import math
 
 mylist = [0, 10, -6, -3, 2, 1]
 a = list(map(lambda x: abs(x), mylist[mylist.index(0):] ))
-print(sum(a), min(a[a.index(1):]))
+print(sum(a), min(a[1:]))
